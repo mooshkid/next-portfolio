f5014924306fa5407adf76567edf0ea0172482a4
masa yamanaka <mooshkid@gmail.com>
2023-05-23 14:31:15 +0900

    updated Hero.tsx and added resume pdf

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,25 +1,38 @@
-'use client';
+"use client";
 
 import React from "react";
 import Image from "next/image";
 import { Container, Flex, Heading, Stack, Text, useColorModeValue } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { Link } from "@chakra-ui/next-js";
+import { DownloadIcon, ExternalLinkIcon } from "@chakra-ui/icons";
 
+interface Button {
+  text: string;
+  link: string;
+  icon: keyof typeof icons;
+}
 
-const cta = [
+const buttons: Button[] = [
   {
     text: "GitHub",
-    link: "https://github.com/mooshkid/"
+    link: "https://github.com/mooshkid/",
+    icon: "ExternalLinkIcon",
   },
   {
     text: "Resume",
-    link: "#",
+    link: "/Masa_resume.pdf",
+    icon: "DownloadIcon",
   },
-]
+];
+
+const icons = {
+  ExternalLinkIcon: <ExternalLinkIcon />,
+  DownloadIcon: <DownloadIcon />,
+};
 
 function generateLinks() {
-  return cta.map((item, index) => (
+  return buttons.map((item, index) => (
     <Link
       as={NextLink}
       key={index}
@@ -30,19 +43,31 @@ function generateLinks() {
       borderRadius="5px"
       border="1px"
       borderColor={useColorModeValue("#ff4d4d", "white")}
-      _hover={{ textDecoration: "none", boxShadow: useColorModeValue("0 0 15px #f9cb28", "0 0 15px #007cf0") }}
+      _hover={{
+        textDecoration: "none",
+        boxShadow: useColorModeValue("0 0 15px #f9cb28", "0 0 15px #007cf0"),
+      }}
     >
-      {item.text}
+      <Flex gap="2" alignItems="center">
+        {item.text}
+        {icons[item.icon]}
+      </Flex>
     </Link>
   ));
 }
 
+
 const Hero = () => {
   return (
     <Container maxW={"7xl"} mx={"auto"}>
-      <Stack id="hero" minH={{ md: "70vh"}} justify={"center"}>
-        <Flex align={"center"} justify={"center"} mx={"auto"} direction={{ base: "column", md: "row" }}>
-          <Flex flex={1} order={{ base:"1", md: "2"}}>
+      <Stack id="hero" minH={{ md: "70vh" }} justify={"center"}>
+        <Flex
+          align={"center"}
+          justify={"center"}
+          mx={"auto"}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Flex flex={1} order={{ base: "1", md: "2" }}>
             <Image
               src="https://mooshkid.github.io/assets/slark_icon.png"
               alt="hero image"
@@ -50,28 +75,25 @@ const Hero = () => {
               height={300}
             />
           </Flex>
-          <Stack flex={1} spacing={6} w={"full"} maxW={"lg"} order={{ base:"2", md: "1"}}>
+          <Stack flex={1} spacing={6} w={"full"} maxW={"lg"} order={{ base: "2", md: "1" }}>
             <Heading as="h1" size="3xl">
               Hi, I&apos;m{" "}
-              <Text as="span" bgGradient={useColorModeValue("linear(to-l, #ff4d4d, #f9cb29)", "linear(to-l, #007cf0, #00dfd8)")} bgClip="text">
+              <Text
+                as="span"
+                bgGradient={useColorModeValue(
+                  "linear(to-l, #ff4d4d, #f9cb29)",
+                  "linear(to-l, #007cf0, #00dfd8)"
+                )}
+                bgClip="text"
+              >
                 Masa
               </Text>
             </Heading>
-            <Text fontSize="3xl">Front-end Developer</Text>
-            <Text>Lorem ipsum dolor sit amet.</Text>
+            <Text fontSize="2xl">Front-end Developer</Text>
             <Stack direction="row" spacing="4">
-
-              {/* make this part a map  */}
-              {/* <Link href="" target="_blank" boxShadow={useColorModeValue("0 0 5px #f9cb28", "0 0 5px #007cf0")} p="5px 10px" borderRadius="5px" border="1px" borderColor={useColorModeValue("#ff4d4d", "white")} sx={{_hover: {textDecoration: "none"}}}>Resume</Link>
-              <Link href="https://github.com/mooshkid/" target="_blank" boxShadow={useColorModeValue("0 0 5px #f9cb28", "0 0 5px #007cf0")} p="5px 10px" borderRadius="5px" border="1px" borderColor={useColorModeValue("#ff4d4d", "white")} sx={{_hover: {textDecoration: "none"}}}>
-                GitHub
-              </Link> */}
-              {/* map to here  */}
-              
               {generateLinks()}
             </Stack>
           </Stack>
-
         </Flex>
       </Stack>
     </Container>

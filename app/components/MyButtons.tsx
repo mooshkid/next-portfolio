d97c36eb017abed6eef7ef0a033d6d7d6674ba92
masa yamanaka <mooshkid@gmail.com>
2023-06-01 13:21:13 +0900

    changed useColorModeValue to _dark

--- app/components/MyButtons.tsx
+++ app/components/MyButtons.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { LinkIcon } from "@chakra-ui/icons";
-import { Flex, Link, Stack, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Link, Stack } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 const MyButtons = ({ buttons }) => {
@@ -18,13 +18,20 @@ const MyButtons = ({ buttons }) => {
               key={index}
               href={item.link}
               isExternal
-              boxShadow={useColorModeValue("0 0 5px #f9cb28", "0 0 5px #007cf0")}
+              boxShadow="0 0 5px #f9cb28"
               p="5px 10px"
               borderRadius="5px"
               border="1px"
-              borderColor={useColorModeValue("#ff4d4d", "white")}
+              borderColor="#ff4d4d"
               _hover={{
-                boxShadow: useColorModeValue("0 0 15px #f9cb28", "0 0 15px #007cf0"),
+                boxShadow: "0 0 15px #f9cb28",
+              }}
+              _dark={{
+                borderColor: "white",
+                boxShadow: "0 0 5px rgba(0, 124, 240, 1)",
+                _hover: {
+                  boxShadow: "0 0 15px rgba(0, 124, 240, 1)",
+                }
               }}
             >
               <Flex gap="2" alignItems="center">
